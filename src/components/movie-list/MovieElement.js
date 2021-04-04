54e5013bb66b2a7974dc60bb39edade7f46bbfcc
Hassan <hassan.alirobleh@gmail.com>
2021-04-04 21:44:33 +0200

    Refactorisation de l'application

--- src/components/movie-list/MovieElement.js
+++ src/components/movie-list/MovieElement.js
@@ -3,12 +3,13 @@ import React, { Component } from "react";
 export default class MovieElement extends Component {
   render() {
     return (
-        <div className="w-25 p-2">
-          <div className="card">
-              <img src="https://www.gstatic.com/tv/thumb/v22vodart/24674/p24674_v_v8_ag.jpg" className="card-img-top" alt="film" />
-              <div className="card-body">
-                <h5 className="card-title">GLADIATOR</h5>
-                <p className="card-text">Le général romain Maximus est le plus fidèle soutien de l'empereur Marc Aurèle, qu'il a conduit de victoire en victoire avec une bravoure et un dévouement exemplaires.</p>
+        <div className="w-50 p-2">
+          <div className="border d-flex">
+              <img width="150" height="200" src="https://www.gstatic.com/tv/thumb/v22vodart/24674/p24674_v_v8_ag.jpg" alt="film" />
+              <div className="flex-fill d-flex flex-column p-3">
+                <h5>Titre</h5>
+                <hr className="w-100" />
+                <p>Le général romain Maximus est le plus fidèle soutien de l'empereur Marc Aurèle, qu'il a conduit de victoire en victoire avec une bravoure et un dévouement exemplaires.</p>
               </div>
           </div>
         </div>
