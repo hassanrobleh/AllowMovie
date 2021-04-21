7bcd5638ec4ff9ddb418fd977532574dfea56c9e
Hassan <hassan.alirobleh@gmail.com>
2021-04-21 21:47:00 +0200

    Création du store

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import reducers from "./reducers";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunkMiddleware from "redux-thunk";
+
+const appReducer = combineReducers(reducers);
+
+const middleware = [thunkMiddleware];
+
+if (ProcessingInstruction.env.NODE_ENV === "development") {
+  middleware.push(logger);
+}
+
+export const store = createStore(
+  appReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
